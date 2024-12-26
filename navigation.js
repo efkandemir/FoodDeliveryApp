8f304730089c291d945bd50a1ac214a1658b440c
Efkan Demir <demirefkan33@gmail.com>
2024-12-26 17:44:04 +0300

    FoodDeliveryApplication first process completed

---
+++ navigation.js
@@ -0,0 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
+import React from 'react';
+import HomeScreen from "./screens/HomeScreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
+
+export default function navigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown:false}}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
